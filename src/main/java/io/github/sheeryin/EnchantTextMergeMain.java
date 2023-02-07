e601e8eef7fcb52ec5fd90854ee458640fa584e7
SheerYin <sheerhuangjinyin@gmail.com>
2023-02-07 23:29:09 +0800

    ..

--- src/main/java/io/github/sheeryin/EnchantTextMergeMain.java
+++ src/main/java/io/github/sheeryin/EnchantTextMergeMain.java
@@ -4,6 +4,8 @@ import io.github.sheeryin.commands.EnchantTextMergeCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 public class EnchantTextMergeMain extends JavaPlugin {
@@ -19,6 +21,20 @@ public class EnchantTextMergeMain extends JavaPlugin {
         instance = this;
         Bukkit.getLogger().info("§f[§e附魔合并§f] 插件开始加载");
         Objects.requireNonNull(this.getCommand("enchanttextmerge")).setExecutor(new EnchantTextMergeCommand());
+
+        File folder = new File(this.getDataFolder(), "test");
+        if (!folder.exists()) {
+            folder.mkdirs();
+        } else {
+            // 文件夹已存在，取消操作
+            return;
+        }
+
+        File test = new File(folder, "test.yml");
+        Bukkit.getLogger().info(test.toString());
+        this.saveResource(test.getName(), true);
+
+
     }
 
     public void onDisable() {
